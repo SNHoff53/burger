21c137ed4bb10f1dc0625793f7f12f2c0523543a
hoffs <stephanie.nhoffman7@gmail.com>
2019-12-03 17:48:14 -0800

    set up MYSQL connection and module.export to use in our ORM file

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Setting up MySQL connection
+var mysql = require("mysql");
+
+// Setting up the connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: '',
+    database: "burgers_db"
+});
+
+// Making the connection
+connection.connect(function(err) { 
+    if (err) {
+        console.log("Sorry, there is an error connecting: " + err.stack + "." + "Please check your connection.");
+        return;
+    }
+    console.log("Connection was successful. Connected as id " + connection.threadId);
+});
+
+// Exporting the connection for our ORM to use
+module.exports = connection;
